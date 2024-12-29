53bed8d32eabf068caa5f5df3bbebc05cd4d6036
Norman <normanglad26012004@gmail.com>
2024-12-29 13:37:17 -0500

    certificated included
    https requests work

--- server/index.js
+++ server/index.js
@@ -1,29 +1,34 @@
 const express = require('express');
+const https = require('https')
+
 const app = express();
 const cors = require('cors');
+const fs = require('fs')
 const { default: mongoose } = require('mongoose');
 require('dotenv').config()
 const router = require('./router/routes')
 
+// SSL certificate and key
+const options = {
+    key: fs.readFileSync("ssl/private-key.pem"),
+    cert: fs.readFileSync("ssl/certificate.pem"),
+};
 // Mongo DB Connections
 mongoose.connect(process.env.URL, {
-}).then(response=>{
+}).then(response => {
     console.log('MongoDB Connection Succeeded.')
-}).catch(error=>{
+}).catch(error => {
     console.log('Error in DB connection: ' + error)
 });
-
 
 // Middleware Connections
 app.use(cors())
 app.use(express.json())
 
 // Routes
-app.use('/api/url',router)
+app.use('/api/url', router)
 
-// Connection
-app.listen(process.env.PORT, ()=>{
-    console.log('App running in port: '+process.env.PORT)
-})
-
-// `${process.env.URL}`
+// Create HTTPS server
+https.createServer(options, app).listen(process.env.PORT, () => {
+    console.log('HTTPS server running on port: ' + process.env.PORT);
+});
